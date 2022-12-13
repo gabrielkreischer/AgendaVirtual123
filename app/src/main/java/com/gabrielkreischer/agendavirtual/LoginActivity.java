33bd97c549a9dde02d1f7050e55c9e1040c9abba
gabri <gabrielkreischer@gmail.com>
2022-12-13 20:04:47 -0300

    1.1

--- app/src/main/java/com/gabrielkreischer/agendavirtual/LoginActivity.java
+++ app/src/main/java/com/gabrielkreischer/agendavirtual/LoginActivity.java
@@ -59,10 +59,10 @@ public class LoginActivity extends AppCompatActivity {
                 edtPassword.setText("");
             }
         });
-        btnCancel.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                abrirCadastro();
             }
         });
         txtRecoverPassword.setOnClickListener(new View.OnClickListener() {
@@ -73,27 +73,33 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-    private void login (String email, String senha){
-        mAuth.signInWithEmailAndPassword(email,senha)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("TAG", "signInWithCustomToken:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    Toast.makeText(LoginActivity.this, "Login efetuado com sucesso", Toast.LENGTH_LONG).show();
-                    //redireciona para a proxima activit
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("TAG", "signInWithCustomToken:failure", task.getException());
-                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    private void login (String email, String senha) {
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCustomToken:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Login efetuado com sucesso", Toast.LENGTH_LONG).show();
+                            //redireciona para a proxima activit
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCustomToken:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+    private void abrirCadastro(){
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
